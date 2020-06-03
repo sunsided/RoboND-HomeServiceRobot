70a8193fd9aac8c32a722c56811d01e132ef1ea0
Markus Mayer <widemeadows@gmail.com>
2020-06-03 15:07:20 +0200

    Add navigation test script

---
+++ test_navigation.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+
+xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 5
+
+xterm  -e  "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch"
