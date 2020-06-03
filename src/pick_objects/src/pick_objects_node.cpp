f5d1dd05b62ede15560cd226a85b6887aa952065
Markus Mayer <widemeadows@gmail.com>
2020-06-03 17:05:14 +0200

    Change node name to pick_objects

--- src/pick_objects/src/pick_objects_node.cpp
+++ src/pick_objects/src/pick_objects_node.cpp
@@ -8,7 +8,7 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 
 int main(int argc, char **argv) {
     // Initialize the simple_navigation_goals node
-    ros::init(argc, argv, "simple_navigation_goals");
+    ros::init(argc, argv, "pick_objects");
 
     //tell the action client that we want to spin a thread by default
     MoveBaseClient ac("move_base", true);
