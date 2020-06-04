1df5137d472ffbd969f999758b9cf269cf7f010f
Markus Mayer <widemeadows@gmail.com>
2020-06-04 22:49:37 +0200

    Update pick_objects.sh for new world/map

--- src/scripts/pick_objects.sh
+++ src/scripts/pick_objects.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-echo $(dirname "$0")/../..
+ROOT_DIR=$(dirname "$0")/../..
 
-xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+# See also: http://wiki.ros.org/roslaunch/XML/arg#Passing_an_argument_via_the_command-line
+export TURTLEBOT_GAZEBO_WORLD_FILE=$(readlink -f "${ROOT_DIR}/src/world/world-1.world")
+export TURTLEBOT_GAZEBO_MAP_FILE=$(readlink -f "${ROOT_DIR}/src/map/world-1.yaml")
+
+echo "Using world: ${TURTLEBOT_GAZEBO_WORLD_FILE}"
+echo "Using map:   ${TURTLEBOT_GAZEBO_MAP_FILE}"
+
+xterm -e "source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 
-xterm  -e  "source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e "source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
 
-xterm  -e  "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
 
-xterm  -e "source devel/setup.bash; rosrun pick_objects pick_objects_node"
+xterm -e "source devel/setup.bash; rosrun pick_objects pick_objects_node"
