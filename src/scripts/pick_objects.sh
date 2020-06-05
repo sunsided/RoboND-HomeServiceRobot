a89db32dfb1be87092af96ee325c43be52b56286
Markus Mayer <widemeadows@gmail.com>
2020-06-05 03:12:22 +0200

    Fix pick_objects.sh code

--- src/scripts/pick_objects.sh
+++ src/scripts/pick_objects.sh
@@ -4,8 +4,12 @@ set -euxo pipefail
 ROOT_DIR=$(dirname "$0")/../..
 
 # See also: http://wiki.ros.org/roslaunch/XML/arg#Passing_an_argument_via_the_command-line
-export TURTLEBOT_GAZEBO_WORLD_FILE=$(readlink -f "${ROOT_DIR}/src/world/world-1.world")
-export TURTLEBOT_GAZEBO_MAP_FILE=$(readlink -f "${ROOT_DIR}/src/map/world-1.yaml")
+TURTLEBOT_GAZEBO_WORLD_FILE=$(readlink -f "${ROOT_DIR}/src/world/world-1.world")
+TURTLEBOT_GAZEBO_MAP_FILE=$(readlink -f "${ROOT_DIR}/src/map/world-1.yaml")
+
+# See https://github.com/koalaman/shellcheck/wiki/SC2155
+export TURTLEBOT_GAZEBO_WORLD_FILE
+export TURTLEBOT_GAZEBO_MAP_FILE
 
 echo "Using world: ${TURTLEBOT_GAZEBO_WORLD_FILE}"
 echo "Using map:   ${TURTLEBOT_GAZEBO_MAP_FILE}"
